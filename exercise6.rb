8701bbb9b44f1d03bc6deff176d83a90bfb30b9d
poniatna <poniatna@users.noreply.github.com>
2015-01-29 18:03:46 -0500

    upss i forgot salt, salt added

--- exercise6.rb
+++ exercise6.rb
@@ -3,4 +3,5 @@ end
 
 grocery_list = ["carrots", "celery root", "parsley root", "onions", "garlic", "all spice", "safron", "bay leaf", "pepper", "chicken"]
 grocery_list << "salt"
+
 puts grocery_list
