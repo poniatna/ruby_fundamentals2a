4494bbc4e2e6b0e88026faa500205a4ae4c5dfe2
poniatna <poniatna@users.noreply.github.com>
2015-01-29 18:00:54 -0500

    grocery_list created

---
+++ exercise6.rb
@@ -0,0 +1,6 @@
+def chicken_soup
+end
+
+grocery_list = ["carrots", "celery root", "parsley root", "onions", "garlic", "all spice", "safron", "bay leaf", "pepper", "chicken"]
+grocery_list << "salt"
+puts grocery_list
