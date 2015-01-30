08cd23c07ce9b9bdca11ccf1d1108df8c6e8381a
poniatna <poniatna@users.noreply.github.com>
2015-01-29 19:13:00 -0500

    items counted in Array

--- exercise6.rb
+++ exercise6.rb
@@ -1,10 +1,11 @@
 require 'pry'
-
+puts " \nchicken_soup\n ruby"
 grocery_list = ["carrots", "celery root", "parsley root", "onions", "garlic", "all spice", "safron", "bay leaf", "pepper", "chicken"]
 grocery_list << "salt"
 grocery_list.each {|x| puts " * #{x}" }
+puts "You have #{grocery_list.length} items on your list"
 
-end
-puts chicken_soup
+
+
 
 
