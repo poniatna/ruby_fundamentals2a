d5041b8a1476f671a87cf2d6cce40ad050f77200
poniatna <poniatna@users.noreply.github.com>
2015-01-29 19:29:15 -0500

    banana issue

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,14 @@
 require 'pry'
 puts " \nchicken_soup\n ruby"
-grocery_list = ["carrots", "celery root", "parsley root", "onions", "garlic", "all spice", "safron", "bay leaf", "pepper", "chicken"]
-grocery_list << "salt"
-grocery_list.each {|x| puts " * #{x}" }
-puts "You have #{grocery_list.length} items on your list"
+	grocery_list = ["carrots", "celery root", "parsley root", "onions", "garlic", "all spice", "safron", "bay leaf", "pepper", "chicken"]
+	grocery_list << "salt"
+	grocery_list.each {|x| puts " * #{x}" }
+puts "You have #{grocery_list.length} items on your list.\n" 
+
+unless grocery_list.include?("bananas")
+	 puts "Luckily, there is no bananas on your list\nwho puts bananas in 'chicken soup' anyway?"
+end
+
 
 
 
