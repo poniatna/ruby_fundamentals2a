8b81c062df26445a1de6e27f558ada060a623e43
poniatna <poniatna@users.noreply.github.com>
2015-01-29 19:51:20 -0500

    sorted alphabeticaly

--- exercise6.rb
+++ exercise6.rb
@@ -3,13 +3,15 @@ puts " \nchicken_soup\n ruby"
 	grocery_list = ["carrots", "celery root", "parsley root", "onions", "garlic", "all spice", "safron", "bay leaf", "pepper", "chicken"]
 	grocery_list << "salt"
 	grocery_list.each {|x| puts " * #{x}" }
-puts "You have #{grocery_list.length} items on your list.\n" 
+puts "You have #{grocery_list.length} items on your list.\n " 
 
 unless grocery_list.include?("bananas")
-	 puts "Luckily, there is no bananas on your list\nwho puts bananas in 'chicken soup' anyway?"
+	 puts "Luckily, there is no bananas on your list\nwho puts bananas in 'chicken soup' anyway?\n "
 end
 
-puts " #{grocery_list[1].capitalize} is second on your list."	
+puts " #{grocery_list[1].capitalize} is second on your list.\n "	
+puts " \nMaybe it would be easier for you to see it in alphabetical order?\n "
+grocery_list.sort.each{|word| puts word}
 
 
 
